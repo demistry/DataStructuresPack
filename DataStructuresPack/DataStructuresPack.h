fc88663c763f816276454179b58aa2e1a75a1d3b
Demistry <dave11ilen@gmail.com>
2019-05-22 18:41:29 +0100

    Initial Commit

---
+++ DataStructuresPack/DataStructuresPack.h
@@ -0,0 +1,19 @@
+//
+//  DataStructuresPack.h
+//  DataStructuresPack
+//
+//  Created by David Ilenwabor on 22/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DataStructuresPack.
+FOUNDATION_EXPORT double DataStructuresPackVersionNumber;
+
+//! Project version string for DataStructuresPack.
+FOUNDATION_EXPORT const unsigned char DataStructuresPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructuresPack/PublicHeader.h>
+
+
---
+++ DataStructuresPackTests/DataStructuresPackTests.swift
@@ -0,0 +1,34 @@
+//
+//  DataStructuresPackTests.swift
+//  DataStructuresPackTests
+//
+//  Created by David Ilenwabor on 22/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import XCTest
+@testable import DataStructuresPack
+
+class DataStructuresPackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
